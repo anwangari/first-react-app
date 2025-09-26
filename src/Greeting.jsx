7a2aefc2c957101c3bb30350440ed12a98de8009
Anthony Njuguna <8034679+anwangari@users.noreply.github.com>
2025-09-26 07:15:02 +0300

    new file:   src/Greeting.jsx new file:   src/Something.jsx modified:   src/main.jsx
    Added new practice React components.

---
+++ src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+    return <h1>Hello, World User!</h1>
+}
+
+export default Greeting
---
+++ src/Something.jsx
@@ -0,0 +1,15 @@
+function Something() {
+    return (
+        <>
+            <h2>Something New in React</h2>
+            <svg>
+                <circle cx="135" cy="75" r="30" stroke="green" strokeWidth="3" />
+            </svg>
+            <form>
+                <input type="text" />
+            </form>
+
+        </>
+    )
+}
+export {Something}
